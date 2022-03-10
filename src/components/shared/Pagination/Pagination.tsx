35e98d6416cf65d500b7a5a1922e8e2124288ca0
Radoslav Tsanev <tsanev96@gmail.com>
2022-03-11 00:48:23 +0200

    click events added to the icon buttons and checking if btns should be disabled

--- src/components/shared/Pagination/Pagination.tsx
+++ src/components/shared/Pagination/Pagination.tsx
@@ -1,4 +1,4 @@
-import { IconButton, Grid } from "@mui/material";
+import { IconButton, Grid, Typography } from "@mui/material";
 import FirstPageIcon from "@mui/icons-material/FirstPage";
 import LastPageIcon from "@mui/icons-material/LastPage";
 import PreviousPageIcon from "@mui/icons-material/KeyboardArrowLeft";
@@ -11,26 +11,27 @@ interface IProps {
   onPageChange: (page: number) => void;
 }
 
-const Pagination = ({ currentPage, pages, isPositionStart, onPageChange }: IProps) => {
+const Pagination = ({ currentPage, pages, isPositionStart = false, onPageChange }: IProps) => {
 
-  const handleFirstPage = () => onPageChange(1)
-  const handleLastPage = () => onPageChange(pages)
-  const handlePreviousPage = (page: number) => onPageChange(page === 0 ? 1 : page)
-  const handleNextPage = (page: number) => onPageChange(page > pages ? page - 1 : page)
+  const handleFirstPage = () => onPageChange(1);
+  const handleLastPage = () => onPageChange(pages);
+  const handlePreviousPage = (page: number) => onPageChange(page === 0 ? 1 : page);
+  const handleNextPage = (page: number) => onPageChange(page > pages ? page - 1 : page);
 
   return (
-    <Grid container justifyContent={isPositionStart ? "flex-start" : "flex-end"}>
-      <IconButton onClick={handleFirstPage}>
+    <Grid container justifyContent={isPositionStart ? "flex-start" : "flex-end"} alignItems="center">
+      <Typography style={{ fontWeight: 500 }}>{currentPage} of {pages}</Typography>
+      <IconButton disabled={currentPage === 1} onClick={handleFirstPage}>
         <FirstPageIcon />
       </IconButton>
-      <IconButton>
-        <PreviousPageIcon onClick={() => handlePreviousPage(currentPage - 1)} />
+      <IconButton disabled={currentPage === 1} onClick={() => handlePreviousPage(currentPage - 1)}>
+        <PreviousPageIcon />
       </IconButton>
-      <IconButton>
-        <NextPageIcon onClick={() => handleNextPage(currentPage + 1)} />
+      <IconButton disabled={currentPage === pages} onClick={() => handleNextPage(currentPage + 1)}>
+        <NextPageIcon />
       </IconButton>
-      <IconButton>
-        <LastPageIcon onClick={handleLastPage} />
+      <IconButton disabled={currentPage === pages} onClick={handleLastPage}>
+        <LastPageIcon />
       </IconButton>
     </Grid>
   );
